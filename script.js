0a6fd5d7a6cd9d8dbd78d1915d10cbd0ce37f7d3
pamellam <38889625+pamellam@users.noreply.github.com>
2024-04-29 17:17:32 +0300

    fixed minor bug

--- script.js
+++ script.js
@@ -4,19 +4,31 @@ const notificationText = document.querySelector('.notification__mark p');
 
 notificationCount.textContent = unreadMsg.length;
 
+// Function to update the notification count
+function updateNotificationCount() {
+  notificationCount.textContent = unreadMsg.length;
+}
+
 // Event listener for each unread message
 unreadMsg.forEach((message) => {
   message.addEventListener('click', () => {
-    message.classList.remove('unread');
-    unreadMsg.length--;
-    notificationCount.textContent = unreadMsg.length;
+    if (message.classList.contains('unread')) {
+      // If the message is unread, remove the 'unread' class
+      message.classList.remove('unread');
+      // Remove the message from the unreadMsg array
+      const index = unreadMsg.indexOf(message);
+      if (index !== -1) {
+        unreadMsg.splice(index, 1);
+        updateNotificationCount();
+      }
+    }
   });
 });
 
+// Remove all
 notificationText.addEventListener('click', () => {
   unreadMsg.forEach((message) => {
     message.classList.remove('unread');
-
-    notificationCount.textContent = 0;
   });
+  notificationCount.textContent = 0;
 });
